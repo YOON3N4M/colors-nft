4ed1063b0e6d9a28892da2a4ff6c781932d959a1
yoon3nam <lgmysn2@gmail.com>
2024-03-06 14:50:31 +0900

    fix: 빌드 에러 수정
    map 함수 key 누락

--- src/containers/shop/FilterColor.tsx
+++ src/containers/shop/FilterColor.tsx
@@ -1,116 +1,118 @@
-import { ColorCardType } from '@/components/ColorCard'
-import { hue, lightness } from '@/constants/colors'
-import { Box, Flex } from '@chakra-ui/react'
+import { ColorCardType } from "@/components/ColorCard";
+import { hue, lightness } from "@/constants/colors";
+import { Box, Flex } from "@chakra-ui/react";
 
 interface FilterColorProps {
-	type: ColorCardType
-	filterLightness: string | null
-	filterHue: string | null
-	setType: React.Dispatch<React.SetStateAction<ColorCardType>>
-	setFilterLightness: React.Dispatch<React.SetStateAction<string | null>>
-	setFilterHue: React.Dispatch<React.SetStateAction<string | null>>
+  type: ColorCardType;
+  filterLightness: string | null;
+  filterHue: string | null;
+  setType: React.Dispatch<React.SetStateAction<ColorCardType>>;
+  setFilterLightness: React.Dispatch<React.SetStateAction<string | null>>;
+  setFilterHue: React.Dispatch<React.SetStateAction<string | null>>;
 }
 
 function FilterColor(props: FilterColorProps) {
-	const {
-		type,
-		filterHue,
-		filterLightness,
-		setType,
-		setFilterLightness,
-		setFilterHue,
-	} = props
+  const {
+    type,
+    filterHue,
+    filterLightness,
+    setType,
+    setFilterLightness,
+    setFilterHue,
+  } = props;
 
-	const colorTransition = {
-		transition: 'color',
-		transitionDuration: '200ms',
-	}
+  const colorTransition = {
+    transition: "color",
+    transitionDuration: "200ms",
+  };
 
-	return (
-		<Box>
-			<Box>
-				<Flex>
-					<Box>style</Box>
-					<Flex ml={1} gap={2}>
-						<Box
-							as="button"
-							color={type === 'card' ? 'black' : '#808080b8'}
-							_hover={{ color: 'black' }}
-							__css={colorTransition}
-							onClick={() => setType('card')}
-							fontStyle={'italic'}
-						>
-							card
-						</Box>
-						<Box
-							as="button"
-							color={type === 'square' ? 'black' : '#808080b8'}
-							_hover={{ color: 'black' }}
-							__css={colorTransition}
-							onClick={() => setType('square')}
-							fontStyle={'italic'}
-						>
-							square
-						</Box>
-						<Box
-							as="button"
-							color={type === 'text' ? 'black' : '#808080b8'}
-							_hover={{ color: 'black' }}
-							__css={colorTransition}
-							onClick={() => setType('text')}
-							fontStyle={'italic'}
-						>
-							text
-						</Box>
-					</Flex>
-				</Flex>
-				<Flex>
-					<Box>lightness</Box>
-					<Flex ml={1} gap={2}>
-						<Box
-							as="button"
-							color={filterLightness === null ? 'black' : '#808080b8'}
-							_hover={{ color: 'black' }}
-							__css={colorTransition}
-							onClick={() => setFilterLightness(null)}
-							fontStyle={'italic'}
-						>
-							all
-						</Box>
-						{lightness.map((level) => (
-							<Box
-								as="button"
-								color={filterLightness === level ? 'black' : '#808080b8'}
-								_hover={{ color: 'black' }}
-								__css={colorTransition}
-								onClick={() => setFilterLightness(level)}
-								fontStyle={'italic'}
-							>
-								{level}
-							</Box>
-						))}
-					</Flex>
-				</Flex>
-				<Flex>
-					<Box>hue</Box>
-					<Flex ml={1} columnGap={2} flexWrap={'wrap'}>
-						{hue.map((level) => (
-							<Box
-								as="button"
-								color={filterHue === level ? 'black' : '#808080b8'}
-								_hover={{ color: 'black' }}
-								__css={colorTransition}
-								onClick={() => setFilterHue(level)}
-								fontStyle={'italic'}
-							>
-								{level}
-							</Box>
-						))}
-					</Flex>
-				</Flex>
-			</Box>
-		</Box>
-	)
+  return (
+    <Box>
+      <Box>
+        <Flex>
+          <Box>style</Box>
+          <Flex ml={1} gap={2}>
+            <Box
+              as="button"
+              color={type === "card" ? "black" : "#808080b8"}
+              _hover={{ color: "black" }}
+              __css={colorTransition}
+              onClick={() => setType("card")}
+              fontStyle={"italic"}
+            >
+              card
+            </Box>
+            <Box
+              as="button"
+              color={type === "square" ? "black" : "#808080b8"}
+              _hover={{ color: "black" }}
+              __css={colorTransition}
+              onClick={() => setType("square")}
+              fontStyle={"italic"}
+            >
+              square
+            </Box>
+            <Box
+              as="button"
+              color={type === "text" ? "black" : "#808080b8"}
+              _hover={{ color: "black" }}
+              __css={colorTransition}
+              onClick={() => setType("text")}
+              fontStyle={"italic"}
+            >
+              text
+            </Box>
+          </Flex>
+        </Flex>
+        <Flex>
+          <Box>lightness</Box>
+          <Flex ml={1} gap={2}>
+            <Box
+              as="button"
+              color={filterLightness === null ? "black" : "#808080b8"}
+              _hover={{ color: "black" }}
+              __css={colorTransition}
+              onClick={() => setFilterLightness(null)}
+              fontStyle={"italic"}
+            >
+              all
+            </Box>
+            {lightness.map((level) => (
+              <Box
+                key={`lightness-${level}`}
+                as="button"
+                color={filterLightness === level ? "black" : "#808080b8"}
+                _hover={{ color: "black" }}
+                __css={colorTransition}
+                onClick={() => setFilterLightness(level)}
+                fontStyle={"italic"}
+              >
+                {level}
+              </Box>
+            ))}
+          </Flex>
+        </Flex>
+        <Flex>
+          <Box>hue</Box>
+          <Flex ml={1} columnGap={2} flexWrap={"wrap"}>
+            {hue.map((level) => (
+              <Box
+                key={`hue-${level}`}
+                as="button"
+                color={filterHue === level ? "black" : "#808080b8"}
+                _hover={{ color: "black" }}
+                __css={colorTransition}
+                onClick={() => setFilterHue(level)}
+                fontStyle={"italic"}
+              >
+                {level}
+              </Box>
+            ))}
+          </Flex>
+        </Flex>
+      </Box>
+    </Box>
+  );
 }
 
-export default FilterColor
+export default FilterColor;
