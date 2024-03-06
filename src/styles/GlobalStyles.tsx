4bdb5e64f9171a7670bb61a1a18545c104386eec
yoon3nam <lgmysn2@gmail.com>
2024-03-06 10:28:33 +0900

    fix:

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,42 +1,42 @@
-import { Global, css } from '@emotion/react'
+import { Global, css } from "@emotion/react";
 
 const style = css`
-	@font-face {
-		font-family: 'Arita-dotum-Medium';
-		src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_one@1.0/Arita-dotum-Medium.woff')
-			format('woff');
-		font-weight: normal;
-		font-style: normal;
-	}
+  @font-face {
+    font-family: "Arita-dotum-Medium";
+    src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_one@1.0/Arita-dotum-Medium.woff")
+      format("woff");
+    font-weight: normal;
+    font-style: normal;
+  }
 
-	@font-face {
-		font-family: 'ChosunSm';
-		src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-04@1.1/ChosunSm.woff')
-			format('woff');
-		font-weight: normal;
-		font-style: normal;
-	}
+  @font-face {
+    font-family: "ChosunSm";
+    src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-04@1.1/ChosunSm.woff")
+      format("woff");
+    font-weight: normal;
+    font-style: normal;
+  }
 
-	html {
-		font-family: 'Pretendard Variable';
-	}
+  html {
+    font-family: "Pretendard Variable";
+  }
 
-	* {
-		font-family: 'Pretendard Variable' !important;
-	}
+  * {
+    font-family: "Pretendard Variable" !important;
+  }
 
-	body {
-	}
+  body {
+  }
 
-	a {
-		text-decoration: none;
-	}
+  a {
+    text-decoration: none;
+  }
 
-	a:hover {
-	}
+  a:hover {
+  }
 
-	//스크롤바 스타일링
-	/* *::-webkit-scrollbar {
+  //스크롤바 스타일링
+  /* *::-webkit-scrollbar {
     width: 5px;
     background-color: #ffffff;
   }
@@ -45,54 +45,50 @@ const style = css`
     border-radius: 10px;
   } */
 
-	.no-scroll-bar::-webkit-scrollbar {
-		display: none; /* Chrome, Safari, Opera*/
-	}
-	.no-scroll-bar {
-		-ms-overflow-style: none; /* IE and Edge */
-		scrollbar-width: none; /* Firefox */
-	}
+  .no-scroll-bar::-webkit-scrollbar {
+    display: none; /* Chrome, Safari, Opera*/
+  }
+  .no-scroll-bar {
+    -ms-overflow-style: none; /* IE and Edge */
+    scrollbar-width: none; /* Firefox */
+  }
 
-	.curve {
-		svg,
-		.background {
-			position: fixed;
+  .curve {
+    svg,
+    .background {
+      position: fixed;
+      height: calc(100vh + 600px);
+      width: 100vw;
+      pointer-events: none;
+      left: 0;
+      top: 0;
+      z-index: 1000;
+    }
 
-			height: calc(100vh + 600px);
+    .background {
+      transition: opacity 0s linear 0.1s;
+    }
 
-			width: 100vw;
+    .route {
+      position: absolute;
 
-			pointer-events: none;
+      left: 50%;
 
-			left: 0;
+      top: 40%;
 
-			top: 0;
-		}
+      color: white;
 
-		.background {
-			transition: opacity 0s linear 0.1s;
-		}
+      font-size: 46px;
 
-		.route {
-			position: absolute;
+      z-index: 3;
 
-			left: 50%;
+      transform: translateX(-50%);
 
-			top: 40%;
-
-			color: white;
-
-			font-size: 46px;
-
-			z-index: 3;
-
-			transform: translateX(-50%);
-
-			text-align: center;
-		}
-	}
-`
+      text-align: center;
+    }
+  }
+`;
 
 export default function GlobalStyles() {
-	return <Global styles={style} />
+  return <Global styles={style} />;
 }
