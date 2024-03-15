603947feedf3a941f441e62f7a27f09e33a41f53
yoon3nam <lgmysn2@gmail.com>
2024-03-15 11:21:53 +0900

    feat: SignOut을 하지 않을 경우 로그인 정보 기억

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,53 @@
 import Layout from "@/components/layout/Layout";
 import ModalProvider from "@/components/modal/ModalProvider";
+import { auth } from "@/firebase";
+import { useAuthActions } from "@/store/authStore";
 import GlobalStyles from "@/styles/GlobalStyles";
 import { theme } from "@/styles/theme";
+import { UserDocument } from "@/types";
+import { getUserDocument } from "@/utils/auth";
 import { ChakraProvider } from "@chakra-ui/react";
+import { onAuthStateChanged, User } from "firebase/auth";
 import { AnimatePresence } from "framer-motion";
 import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
+
+type IndexedDbState = "initial" | "success" | "fail";
 
 export default function App({ Component, pageProps, router }: AppProps) {
+  const { setIsLogin, setUser } = useAuthActions();
+  const [indexedDbState, setIndexedDbState] =
+    useState<IndexedDbState>("initial");
+  const [indexedUser, setIndexedUser] = useState<User | null>(null);
+
+  function checkIndexedDb() {
+    onAuthStateChanged(auth, (user: any) => {
+      if (!user) {
+        setIndexedDbState("fail");
+      } else {
+        setIndexedDbState("success");
+        setIndexedUser(user);
+      }
+    });
+  }
+
+  async function handleAuth() {
+    if (!indexedUser) return;
+    const userDocRes = await getUserDocument(indexedUser.uid);
+    setUser(userDocRes);
+    setIsLogin(true);
+  }
+
+  useEffect(() => {
+    checkIndexedDb();
+  }, []);
+
+  useEffect(() => {
+    if (indexedDbState !== "success") return;
+
+    handleAuth();
+  }, [indexedDbState]);
+
   return (
     <ChakraProvider theme={theme}>
       <Layout>
