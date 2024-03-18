b7ccb02f7b48fbb8bd7efa8faa58148a7414431f
yoon3nam <lgmysn2@gmail.com>
2024-03-18 16:52:08 +0900

    feat: account 페이지 진입시 유저 데이터 최신화

--- src/containers/account/ContainerAccount.tsx
+++ src/containers/account/ContainerAccount.tsx
@@ -1,8 +1,9 @@
 import Contents from "@/components/layout/Contents";
-import { useIsLogin, useUser } from "@/store/authStore";
+import { useAuthActions, useIsLogin, useUser } from "@/store/authStore";
+import { getUserDocument } from "@/utils/auth";
 import { useRouter } from "next/navigation";
 
-import React from "react";
+import React, { useEffect } from "react";
 
 import UserToken from "./UserToken";
 
@@ -10,6 +11,19 @@ export default function ContainerAccount() {
   const router = useRouter();
   const isLogin = useIsLogin();
   const user = useUser();
+
+  const { setUser } = useAuthActions();
+
+  async function updateUser() {
+    if (!user) return;
+    const freshUserDocument = await getUserDocument(user.uid);
+    setUser(freshUserDocument);
+  }
+
+  useEffect(() => {
+    // * 해당 페이지는 토큰, 보유 색상등 데이터 최신화가 필요하기 떄문에 페이지 진입시 데이터 최신화
+    updateUser();
+  }, []);
 
   if (!isLogin) {
     return;
