f3f15e981ba8add42617716d44a4948b7438394b
yoon3nam <lgmysn2@gmail.com>
2024-03-18 13:18:19 +0900

    fix: 하이드레이션 수정

--- src/containers/main/ContainerHome.tsx
+++ src/containers/main/ContainerHome.tsx
@@ -18,43 +18,68 @@ import {
 import { delay, motion } from "framer-motion";
 import React, { useEffect, useState } from "react";
 import { BiSolidColor } from "react-icons/bi";
+import { IoIosInformationCircleOutline } from "react-icons/io";
 import DisplayFlipCard from "./DisplayFlipCard";
 
 export default function ContainerHome() {
   const cardColors = pickColors(["16-4728", "18-2436", "18-0135"]);
+  const [render, setRender] = useState(false);
 
+  useEffect(() => {
+    setRender(true);
+  }, []);
   return (
-    <Contents>
-      <Flex as={"section"} w={"100%"} py={8} justifyContent="space-between">
-        <Box className="left" w={"50%"} py={8} flexWrap="wrap">
-          <Heading fontSize={"6xl"}>
-            Welcome to the <br />
-            <Text display={"inline"} color={`#${cardColors[0].hex}`}>
-              colorful&nbsp;
-            </Text>
-            World
-          </Heading>
-          <Text mt={2}>COLORS are bla blab bla bla</Text>
-          <Flex mt={12} gap={8}>
-            <Button
-              display={"flex"}
-              colorScheme="brand"
-              alignItems={"center"}
-              gap={2}
-            >
-              See the colors <BiSolidColor />
-            </Button>
-            <Button variant={"outline"} colorScheme="brand">
-              More information
-            </Button>
+    <>
+      {render && (
+        <Contents>
+          <Flex
+            flexDirection={{ mo: "column", pc: "row" }}
+            as={"section"}
+            w={"100%"}
+            py={8}
+            justifyContent="space-between"
+          >
+            <Box className="left" w={{ pc: "50%" }} py={8} flexWrap="wrap">
+              <Heading fontSize={"6xl"}>
+                Welcome to the <br />
+                <Text display={"inline"} color={`#${cardColors[0].hex}`}>
+                  colorful&nbsp;
+                </Text>
+                World
+              </Heading>
+              <Text mt={8} fontSize="lg">
+                <Text display={"inline"} fontWeight="600">
+                  Find the your favorite
+                </Text>{" "}
+                out of 3,000 different colors
+              </Text>
+              <Flex mt={12} gap={8}>
+                <Button
+                  display={"flex"}
+                  colorScheme="brand"
+                  alignItems={"center"}
+                  gap={2}
+                >
+                  See the colors <BiSolidColor />
+                </Button>
+                <Button
+                  variant={"outline"}
+                  colorScheme="brand"
+                  alignItems={"center"}
+                  gap={2}
+                >
+                  More information <IoIosInformationCircleOutline />
+                </Button>
+              </Flex>
+            </Box>
+            <Box flex={1}>
+              <Box display="flex" justifyContent={"center"}>
+                <DisplayFlipCard colors={cardColors} />
+              </Box>
+            </Box>
           </Flex>
-        </Box>
-        <Box flex={1}>
-          <Box display="flex" justifyContent={"center"}>
-            <DisplayFlipCard colors={cardColors} />
-          </Box>
-        </Box>
-      </Flex>
-    </Contents>
+        </Contents>
+      )}
+    </>
   );
 }
--- src/containers/main/DisplayFlipCard.tsx
+++ src/containers/main/DisplayFlipCard.tsx
@@ -2,6 +2,7 @@ import FlipCard from "@/components/FlipCard";
 import { ArrangedColor } from "@/types/color";
 import { Box } from "@chakra-ui/react";
 import React, { useState } from "react";
+import { motion } from "framer-motion";
 
 interface DisplayFlipCardProps {
   colors: ArrangedColor[];
@@ -12,23 +13,29 @@ export default function DisplayFlipCard(props: DisplayFlipCardProps) {
   const [hoverd, setHoverd] = useState(0);
 
   return (
-    <Box position={"relative"} display="flex" w="min-content">
-      {colors.map((color, idx) => (
-        <Box
-          onMouseEnter={() => setHoverd(idx)}
-          onMouseLeave={() => setHoverd(0)}
-          position={idx > 0 ? "absolute" : "initial"}
-          left={idx > 0 ? `${idx === 1 ? "20" : "-20"}` : 0}
-          zIndex={hoverd === idx ? 100 : `${90 - idx}`}
-          transform={`rotate(${idx === 1 ? "20" : "-15"}deg) ${
-            idx > 0 && `scale(0.85)`
-          }`}
-          transition="transform 0.2s"
-          _hover={{ transform: "rotate(0)" }}
-        >
-          <FlipCard color={color} />
-        </Box>
-      ))}
-    </Box>
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 1, duration: 0.3, type: "tween" }}
+    >
+      <Box position={"relative"} display="flex" w="min-content">
+        {colors.map((color, idx) => (
+          <Box
+            onMouseEnter={() => setHoverd(idx)}
+            onMouseLeave={() => setHoverd(0)}
+            position={idx > 0 ? "absolute" : "initial"}
+            left={idx > 0 ? `${idx === 1 ? "20" : "-20"}` : 0}
+            zIndex={hoverd === idx ? 100 : `${90 - idx}`}
+            transform={`rotate(${idx === 1 ? "20" : "-15"}deg) ${
+              idx > 0 && `scale(0.85)`
+            }`}
+            transition="transform 0.2s"
+            _hover={{ transform: "rotate(0)" }}
+          >
+            <FlipCard color={color} />
+          </Box>
+        ))}
+      </Box>
+    </motion.div>
   );
 }
